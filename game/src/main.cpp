ac510335b999f8ac00913c354a451486e634c6d3
Liam <rousselle.mail@gmail.com>
2025-12-11 18:39:43 -0700

    Added GLFW support

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
 int main(int argc, char** argv)
 {
+    if ( !glfwInit() )
+        return 1;
+
+    GLFWwindow* myWindow = glfwCreateWindow(1920, 1080, "SCP: Containment Breach  -  C++ Port", nullptr, nullptr);
+    if ( !myWindow )
+        return 1;
+
+
+    while ( !glfwWindowShouldClose(myWindow) )
+    {
+        glfwPollEvents();
+        glClear()
+    }
+
+
+    glfwDestroyWindow(myWindow);
+    glfwTerminate();
     return 0;
 }
